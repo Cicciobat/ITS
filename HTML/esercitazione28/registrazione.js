d4f3e6e97d2bbce7de146020211c85cb79078fc1
Cicciobat <francescomariabattaglia@gmail.com>
2023-03-29 16:11:44 +0200

    Esercitazione del 28 marzo.

---
+++ HTML/esercitazione28/registrazione.js
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2023. Ciccio Battaglia
+ * All rights reserved.
+ *
+ */
+
+function alerts() {
+    alert("Dati inviati con successo.");
+    alert("Verrai reindirizzato alla pagina principale.")
+}
